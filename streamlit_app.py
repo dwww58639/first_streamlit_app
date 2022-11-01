23511a062fcb2be7bdb2ebbd653a5500c5026d82
DWese <116752142+dwww58639@users.noreply.github.com>
2022-11-01 08:09:19 -0400

    Update streamlit_app.py
    separated the fruit name from the URL to prepare to allow for the fruit name to be passed as a parameter.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 #call fruityvice api
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
 
 
 #Normalize the Json response
